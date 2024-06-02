d76efbf4bc9045ef5171f8cbb40829aef07a6000
zhero <zheronkina86@gmail.com>
2024-06-03 00:48:50 +0300

    All project

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
+    private static final String REGEX = "^[a-zA-Z0-9_]*$";
+
+
     public static void main(String[] args) {
+        try {
+            checkCredentials("admin", "111", "1111");
+            System.out.println("Проверка пройдена");
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println("Проверка не пройдена");
+            e.printStackTrace();
+        }
+    }
+
+    public static void checkCredentials(String login, String password, String confirmPassword) {
+        if (login.length() > 20) {
+            throw new WrongLoginException("Длина логина больше 20");
+        }
+
+        if (!login.matches(REGEX)) {
+            throw new WrongLoginException("В логине недопустимые символы");
+        }
+
+        if (password.length() > 20) {
+            throw new WrongPasswordException("Длина пароля больше 20");
+        }
+
+        if (!password.matches(REGEX)) {
+            throw new WrongPasswordException("В пароле недопустимые символы");
+        }
+
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Пароли не совпадают");
+        }
+
     }
 }
